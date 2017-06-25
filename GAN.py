4112ebcf99e000a1f95a2c7b4ae5f7efc06b36fe
td2014 <anthonyd2004@gmail.com>
2017-06-25 16:46:20 -0700

    0.79 training accuracy

--- GAN.py
+++ GAN.py
@@ -25,7 +25,7 @@ K.set_session(sess)
 # End:  Set up environment for reproduction of results
 
 #
-from keras.layers import Dense, Input, concatenate, add
+from keras.layers import Dense, Input, concatenate, add, Dropout
 from keras.models import Sequential, Model
 from keras.datasets import mnist
 
@@ -46,19 +46,20 @@ common_input_dim=x_train_mnist.shape[1]*x_train_mnist.shape[2]
 ###model = Sequential(name='Full_model')
 # Generator portion of model
 main_input = Input(shape=(common_input_dim,), name='main_input')
-x=Dense(128, name='Full_G_Dense_1',input_dim=common_input_dim, use_bias=False)(main_input)
+x=Dense(128, name='Full_G_Dense_1',input_dim=common_input_dim, use_bias=False,)(main_input)
 G_out=Dense(common_input_dim, name='Full_G_Dense_2', use_bias=False)(x)
 # Add input for real data
 auxiliary_input = Input(shape=(common_input_dim,), name='aux_input')
 x = add([G_out, auxiliary_input], name='Full_Add')
 # Discriminator portion (First layer is interface)
-x = Dense(1024, name='Full_D_Dense_1', use_bias=False, trainable=False, kernel_initializer='ones')(x)
-x = Dense(128, name='Full_D_Dense_2', use_bias=True, activation='relu')(x)
-x = Dense(128, name='Full_D_Dense_3', use_bias=True, activation='relu')(x)
-main_output_obj = Dense(1, name='main_output', activation='sigmoid', use_bias=False)(x)
+x = Dense(256, name='Full_D_Dense_1', use_bias=False, trainable=False, kernel_initializer='ones')(x)
+x = Dense(64, name='Full_D_Dense_2', use_bias=True)(x)
+x = Dropout(0.5,name="Full_D_Dropout")(x)
+x = Dense(64, name='Full_D_Dense_3', use_bias=True)(x)
+main_output_obj = Dense(1, name='main_output', activation='sigmoid')(x)
 #
 model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output_obj])
-model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
 print('Baseline:')
 print(model.summary())
 
@@ -71,7 +72,7 @@ layer.trainable = False
 layer = model.get_layer(name='Full_G_Dense_2')
 layer.trainable = False
 # in the model below, the weights of `layers` will not be updated during training
-model.compile(loss='mse', optimizer='rmsprop', metrics=['accuracy'])
+model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
 print('After freeze:')
 model.summary()
 
@@ -80,8 +81,8 @@ model.summary()
 #
 for eLoop in range(1):
 
-    batch_size_loop=20
-    batch_size=2*batch_size_loop # 2x because we add generated and real each step
+    batch_size_loop=100
+    # batch_size=2*batch_size_loop # 2x because we add generated and real each step
     x_train = []
     y_train = []
     x_aux=[]
@@ -90,7 +91,7 @@ for eLoop in range(1):
     for k in range(batch_size_loop):   
         
         # Create input for generator section.
-        np.random.seed(42)
+        #np.random.seed(42)
         x_train_batch=np.random.uniform(low=-0.5,high=0.5,size=(common_input_dim))
         x_aux_batch=np.zeros([common_input_dim])
         y_train_batch=np.zeros([1]) # 0 means G model data
@@ -120,11 +121,13 @@ for eLoop in range(1):
     # Fit model to constructed batch
     model.fit({'main_input': x_train, 'aux_input': x_aux}, 
         {'main_output': y_train}, 
-        epochs=10, batch_size=batch_size)
+        epochs=10, batch_size=40)
     
 #
 # output predictions
 #
+
+prediction = model.predict({'main_input': x_train, 'aux_input': x_aux})
 
 # Real data
 ###x_train=np.zeros([1,common_input_dim])
